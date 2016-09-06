558b1d5cf28da263a5471cc2198acbb18a456db1
chathurangawijesinghe <crwijesinghe@gmail.com>
2016-09-06 05:45:48 +0530

    2. Design_Patterns_Video_Tutorial_2

---
+++ DemoDesignPatterns/Classes/Creature.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDesignPatterns.Classes
+{
+    public abstract class Creature
+    {
+        private string name;
+        private int weight;
+        private string sound;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Weight
+        {
+            get
+            {
+                return weight;
+            }
+
+            set
+            {
+                weight = value;
+            }
+        }
+
+        public string Sound
+        {
+            get
+            {
+                return sound;
+            }
+
+            set
+            {
+                sound = value;
+            }
+        }
+    }
+}
--- DemoDesignPatterns/Classes/WorkWithAnimals.cs
+++ DemoDesignPatterns/Classes/WorkWithAnimals.cs
@@ -8,6 +8,8 @@ namespace DemoDesignPatterns.Classes
 {
     public class WorkWithAnimals
     {
+        int justANumber = 10;
+
         static void Main(string[] args)
         {
             Dog fido = new Dog();
@@ -29,12 +31,34 @@ namespace DemoDesignPatterns.Classes
 
             Console.WriteLine("Dog name after method call : " + fido.Name);
 
+            Animal doggy = new Dog();
+            Animal kitty = new Cat();
+
+            Console.WriteLine("Doggy says : " + doggy.Sound);
+            Console.WriteLine("Kitty says : " + kitty.Sound);
+
+            Animal[] animals = new Animal[4];
+            animals[0] = doggy;
+            animals[1] = kitty;
+
+            Console.WriteLine("Doggy says : " + animals[0].Sound);
+            Console.WriteLine("Kitty says : " + animals[1].Sound);
+
+            SpeakAnimal(doggy);
+
+            ((Dog)doggy).DigHole();
+
             Console.ReadKey();
         }
 
         static void ChangeObejctName(Dog fido)
         { 
             fido.Name = "Marcus";
+        }
+
+        static void SpeakAnimal(Animal animal)
+        {
+            Console.WriteLine("Animal says: " + animal.Sound);
         }
     }
 }
